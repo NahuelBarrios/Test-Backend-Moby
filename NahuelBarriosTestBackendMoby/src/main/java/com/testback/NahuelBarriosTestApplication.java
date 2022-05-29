ebe48499c6a7d93da462d1e27bf84baf4772a141
nahuelbarrios <wbarrios@mobydigital.com>
2022-05-29 14:58:31 -0300

    Iniciando Proyecto

---
+++ NahuelBarriosTestBackendMoby/src/main/java/com/testback/NahuelBarriosTestApplication.java
@@ -0,0 +1,13 @@
+package com.testback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NahuelBarriosTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NahuelBarriosTestApplication.class, args);
+	}
+
+}
