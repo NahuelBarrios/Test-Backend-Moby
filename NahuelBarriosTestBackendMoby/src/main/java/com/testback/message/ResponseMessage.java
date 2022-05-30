d1117fb7bd68505d9e94cfbbfc340f06366d413d
nahuelbarrios <wbarrios@mobydigital.com>
2022-05-29 21:06:44 -0300

    Se agrego exception y message para trabajar las exceptiones de la app

---
+++ NahuelBarriosTestBackendMoby/src/main/java/com/testback/message/ResponseMessage.java
@@ -0,0 +1,16 @@
+package com.testback.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class ResponseMessage {
+    private String message;
+}
