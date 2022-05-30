fb42d5b142f8f8c0031616e317153e118e9cccd8
nahuelbarrios <wbarrios@mobydigital.com>
2022-05-30 13:42:19 -0300

    Se agrega Dtos para pasaje de datos

---
+++ NahuelBarriosTestBackendMoby/src/main/java/com/testback/models/views/CandidateByTechnologyDto.java
@@ -0,0 +1,20 @@
+package com.testback.models.views;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.testback.models.entities.Candidate;
+import com.testback.models.entities.Technology;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CandidateByTechnologyDto {
+    private Long id;
+    private Candidate candidate;
+    private Technology technology;
+    private String experience;
+}
